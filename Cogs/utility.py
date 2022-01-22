35d3614cddcd8512a3c31e300d9f80651d8018b9
R-J Samme <destinydragon890@gmail.com>
2022-01-22 10:39:13 +0000

    Removing Virtual Environment

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -1,6 +1,9 @@
-import discord 
+from os import name
+import discord,json 
 from discord.ext import commands
 import json
+
+from discord.guild import Guild
 
 class Utility(commands.Cog): # Setup bot
     def __init__ (self, bot):
@@ -16,7 +19,7 @@ class Utility(commands.Cog): # Setup bot
         bot_ping = (str(round(self.bot.latency * 1000)) + str(' ms'))
         await ctx.send(bot_ping)
 
-    @commands.command() # D&D weapon damage
+    @commands.command() # D&D weapon damage TODO: Test me
     async def damage(self, ctx, type, weapon):
         database = json.load(open('Media/DnD_weapons.json', 'r'))
 
@@ -25,6 +28,23 @@ class Utility(commands.Cog): # Setup bot
             await ctx.send(f'{weapon.capitalise()} deals {damage}')
         except:
             await ctx.send(f'No weapon called {weapon} in {type} weapons.')
+
+    @commands.command()
+    async def role(self, ctx, mode = '', role_name = '', *, data = ''):
+        if mode.lower() == 'info':
+            role_name = discord.utils.find(lambda m: m.name == role_name, ctx.guild.roles)
+             
+            if role_name == '':
+                await ctx.send('No role found with that name.')
+
+            role_id = Guild.get_role(role_name)
+            await ctx.send(f'Role ID is {role_id}')
+        elif mode.lower() == 'colour':
+            '''change role colour - hex or rgb'''
+        elif mode.lower() == 'members':
+            '''Show members with certain role (embed)'''
+        else:
+            await ctx.send('role command options:\n info - show information about role \ncolour - change colour of a role \nmembers - show members with a role')
 
 
 def setup(bot):
---
+++ Releases/release notes.md
@@ -0,0 +1,18 @@
+# *Changes in this Release:*
+**Full Changelog available at**: https://github.com/Solarapollo05/Discord-Bot-v2/compare/v1.0.0...v1.1
+
+## Change 1:
+### Subtitle 1
+
+- Changed bullet points
+
+## Change 2 
+### Subtitle 2
+
+ ### Reload
+
+- Changes
+
+
+// Replace v1.0.0 and v1.1 with new version names
+// Surround code with `backticks`, and use *astrixes* for bold
