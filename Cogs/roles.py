0cba20422bc542e22a8980fb1dd2a3bde5183b29
Solarapollo05 <destinydragon890@gmail.com>
2022-04-17 21:31:01 +0100

    Role Commands:
    Changed:
    * Added ```info``` command - show basic info about a role.
    * Added ```colour``` command - change colour of a role.
    * Added ```permissions``` command - view catagory permissions for a role.
    * Added ```members``` show all the members with a given role.
    * Added ```bot_name``` in ```config.json``` for correct embed author.

--- Cogs/roles.py
+++ Cogs/roles.py
@@ -8,30 +8,23 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
         self.bot = bot
     class ConfigError(Exception):
         '''Error in provided config file.'''
-        # TODO: Log errors in config file @logging
 
     try:
         config = json.load(open('Cogs/config.json', 'r')) # Load config file, and report error if necessary
+        global BOT_NAME
+        BOT_NAME = config["settings"]["bot_name"]
     except JSONDecodeError as exception:
         config_error = bool(True)
         raise ConfigError('Unable to parse config file')
-
     
-
-
-
-    #
     @commands.command()
     async def info(self, ctx, role : discord.Role):
         
-        # role hex colour, number of users with role, role name, creation date, permission integer
         colour_hex = ('%02x%02x%02x' % (role.colour.r, role.colour.g, role.colour.b))
-        #colour_hex = role.colour.value
         creation_date = role.created_at
         members = role.members
         num_members = len(role.members)
         permissions = role.permissions.value
-        # TODO: Write subprogram to handle permissions objects
 
         def format_members(members):
             if num_members == 1:
@@ -44,12 +37,12 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
                 
 
         embed=discord.Embed(title=f"Role info for {role}", description=f"Info requested by {ctx.author}", color=role.colour) # TODO: mention ctx.author FIXME: colour should not be given in str
-        embed.set_author(name=f"{str(ctx.author.name)}#{str(ctx.author.discriminator)}")
         embed.add_field(name="Colour", value=f"{role.colour.r}, {role.colour.g}, {role.color.b}    #{colour_hex}", inline=True)
         embed.add_field(name="Creation Date", value=creation_date, inline=True)
         embed.add_field(name="Members:", value=format_members(members), inline=False)
         embed.add_field(name="Number of Members", value=num_members, inline=True)
         embed.add_field(name="Permissions Integer", value=permissions, inline=True)
+        embed.set_author(name=BOT_NAME, icon_url="https://cdn.discordapp.com/avatars/811660589037650000/1e8e090bca1c50f583c7f810536366e3.png?size=256")
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -75,7 +68,6 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
                     raw_colour = int(str(raw_colour_hex), 16)
 
                     new_colour = discord.Colour(raw_colour)
-                    """convert from separate r,g,b inputs"""
 
                 try: # Try to edit role
                     reason = f"Colour changed by {ctx.author.name}#{ctx.author.discriminator}"
@@ -84,12 +76,6 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
                 except discord.Forbidden:
                     await ctx.reply("Something went wrong. Do I have permission to edit this role?")
 
-                
-                """TODO: Implement HERE
-                ~# 1 - Check for perms~
-                # 2 - Convert colour to proper format (accept hex, or tuple (r,g,b))
-                ~# 3 - Complain if no colour given~"""
-
         else:
             await ctx.reply ("{ctx.author.mention} You do not have the correct permissions to modify roles.")
 
@@ -97,7 +83,6 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
     async def perms(self, ctx, role : discord.Role, mode=""):
         permissions = role.permissions
         embed=discord.Embed(title=f"Permissions for {role}", description=f"Info requested by {ctx.author}", color=role.colour)
-        embed.set_author(name=f"{str(ctx.author.name)}#{str(ctx.author.discriminator)}")
 
         perms_int = permissions.value
 
@@ -150,11 +135,20 @@ class roles(commands.Cog): # Setup bot (replace template with suitable name)
             embed.add_field(name="Text/Channels:", value="Text channel related permissions", inline=True)
             embed.add_field(name="Admin/Mod:", value="Administrator or Moderator related permissions", inline=True)
 
+        embed.set_author(name=BOT_NAME, icon_url="https://cdn.discordapp.com/avatars/811660589037650000/1e8e090bca1c50f583c7f810536366e3.png?size=256")
         await ctx.send(embed=embed)
 
-    # Events are labeled '@commands.Cog.listener()
-    #
-    #
+    @commands.command()
+    async def members(self, ctx, role : discord.Role):
+        members = role.members
+
+        embed=discord.Embed(title=f"Members with the {role} Role:", description=f"Info requested by {ctx.author}", color=role.colour)
+
+        for member in members:
+            embed.add_field(name=member.display_name, value=f"`{member.name} #{member.discriminator}`", inline=True)
+        
+        embed.set_author(name=BOT_NAME, icon_url="https://cdn.discordapp.com/avatars/811660589037650000/1e8e090bca1c50f583c7f810536366e3.png?size=256")
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(roles(bot))
