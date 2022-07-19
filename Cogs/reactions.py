19698a214725842f822d26759a81d069ec27b8ab
Solarapollo05 <destinydragon890@gmail.com>
2022-07-19 20:09:19 +0100

    Added listener for reaction
    Bugs:
    * Bot does not respond to the on_reaction_add event (for any emojis)

--- Cogs/reactions.py
+++ Cogs/reactions.py
@@ -1,11 +1,25 @@
-import discord
+import discord, re
 from discord.ext import commands
+from json import JSONDecodeError, load
+
+# Variables
+message_record = [] # A list of messages the we have responded to in the past 2 mins
 
 
 class Reactions(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
+    
+    class ConfigError(Exception):
+        '''Error in provided config file.'''
 
+    try:
+        config = load(open('Cogs/config.json', 'r')) # Load config file, and report error if necessary
+        global BOT_NAME
+        BOT_NAME = config["settings"]["bot_name"]
+    except JSONDecodeError as exception:
+        config_error = bool(True)
+        raise ConfigError('Unable to parse config file')
 
     @commands.Cog.listener()
     @commands.bot_has_permissions(manage_messages=True)
@@ -19,15 +33,40 @@ class Reactions(commands.Cog):
                 except discord.Forbidden:
                     await reaction.message.reply("Something went wrong. Do I have the `manage_messages` permission?")
 
-            elif reaction.emoji == "🗑️": # FIXME: Currently broken on windows 8.1 as it does not recoginse this emoji
+            elif reaction.emoji == "🗑️":
                 await reaction.message.delete()
     
     @commands.Cog.listener()
     @commands.bot_has_permissions(manage_messages=True)
     async def on_reaction_clear_emoji(self, reaction:discord.Reaction):
-            if reaction.emoji == "📌" and reaction.message.pinned == True: # TODO: Put pin emoji here
+            if reaction.emoji == "📌" and reaction.message.pinned == True:
                 await reaction.message.unpin(reason=f"Message unpinned due to all pin emojis removed")   
 
 
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction:discord.Reaction, user:discord.Member|discord.User):
+        if reaction.emoji == "🔁":
+            if reaction.message.content.isalnum(): # Check that the message has a number in it
+                value = re.search(r'\d+', reaction.message.content) # Find number in message
+                
+                if value < 45: # Assume F
+                    temp = (int(value) * 1.8) + 32
+                    unit = "celsius"
+                    unit_conv = "fahrenheit"
+                
+                else: # Assume C
+                    temp = (int(value) - 32) / 1.8
+                    unit = "fahrenheit"
+                    unit_conv = "celsius"
+                
+                
+
+                embed=discord.Embed(title="Temperature Conversion", color=0xff0000)
+                embed.add_field(value=f"{value}° {unit} is {temp}° {unit_conv}", inline=False)
+                embed.add_field(value=f"Conversion requested by {user.name}#{user.discriminator}")
+                embed.set_author(name=BOT_NAME, icon_url="https://cdn.discordapp.com/avatars/811660589037650000/1e8e090bca1c50f583c7f810536366e3.png?size=256")
+
+                await reaction.message.reply(embed=embed)
+    
 def setup(bot:commands.Bot):
     bot.add_cog(Reactions(bot))
